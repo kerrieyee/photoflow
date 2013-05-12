d27bd470d060f7d7a4f9da5dc905f00ba5434607
kerrieyee <kerrie@kerrieyee.com>
2013-05-12 12:22:55 -0700

    [UPDATE] Initial commit plus some gem setup and first model created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Photoflow::Application.config.secret_key_base = '591808d2b53558688e82e9a1bbed73645a218129722618c282078c441d7a7fadb72f362a7600d110a3051c4513c3dcfe1b4806bb18f6b74d140e250907331625'
