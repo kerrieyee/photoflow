e2db936159cf0761afc4eb0f8739764f334fe42b
kerrieyee <kerrie@kerrieyee.com>
2013-06-30 15:01:48 -0700

    [TWEAK] updating post factory to reflect the image validations

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,6 +3,6 @@ FactoryGirl.define do
 	factory :post do
 		title {Faker::Name.name}
 		caption {Faker::Lorem.paragraph}
-		photo_url {"http://" + Faker::Internet.domain_name}
+		photo_url {"http://" + Faker::Internet.domain_name + "/" + Faker::Lorem.word + ".jpg"}
 	end
 end
